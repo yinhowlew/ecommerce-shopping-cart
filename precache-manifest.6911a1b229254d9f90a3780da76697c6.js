6d6e5cf229d700d7be05ce728faac6d92ad20054
yinhow <yinhowlew@gmail.com>
2020-03-03 15:05:16 -0500

    Updates

---
+++ precache-manifest.6911a1b229254d9f90a3780da76697c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4f0ddbd787b536a95bd21e7780310e8",
+    "url": "/ecommerce-shopping-cart/index.html"
+  },
+  {
+    "revision": "8871808d27d5142be157",
+    "url": "/ecommerce-shopping-cart/static/css/main.36177ae7.chunk.css"
+  },
+  {
+    "revision": "9a00e2a3ea5cf9680e52",
+    "url": "/ecommerce-shopping-cart/static/js/2.a1148d11.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/ecommerce-shopping-cart/static/js/2.a1148d11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8871808d27d5142be157",
+    "url": "/ecommerce-shopping-cart/static/js/main.987828eb.chunk.js"
+  },
+  {
+    "revision": "a96aa3a8cde899f06154",
+    "url": "/ecommerce-shopping-cart/static/js/runtime-main.b56a03a3.js"
+  }
+]);
